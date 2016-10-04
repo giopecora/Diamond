1d314a46d314c4383acbf07567eeaa3beed53b7a
NickFelix <nicolas.felixa@gmail.com>
2016-10-03 21:40:22 -0300

    Primeira configuração

--- Diamond/App_Start/BundleConfig.cs
+++ Diamond/App_Start/BundleConfig.cs
@@ -29,6 +29,9 @@ namespace Diamond
                 "~/Scripts/app/home.viewmodel.js",
                 "~/Scripts/app/_run.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/SPA").Include(
+                "~/SPA/Controllers/main.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
