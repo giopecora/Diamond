0578285f6a9fe35f5eaa72aabcc6bc0399b154b5
Arthur <arthurluisrodrigues@hotmail.com>
2016-10-05 21:49:27 -0300

    TELA DE CARRINHO DE COMPRA

--- Diamond/Controllers/HomeController.cs
+++ Diamond/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using Diamond.Models;
 
 namespace Diamond.Controllers
 {
@@ -11,6 +13,63 @@ namespace Diamond.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult CarrinhoCompra()
+        {
+            List<CarrinhoCompra> lista = new List<CarrinhoCompra>();
+
+            lista.Add(new CarrinhoCompra()
+            {
+                Imagem = "/Content/img/liquidificador.jpg",
+                Nome = "Liquidificador TX VB BLA",
+                Descricao = "Liquidificar otimizado com 3 quartzos de rosa Juventa, serve para triturar coisas bem duras",
+                Quantidade = 1,
+                ValorUnitario = 78.90,
+                ValorUnitarioTotal = "78,90"
+            });
+
+            lista.Add(new CarrinhoCompra()
+            {
+                Imagem = "/Content/img/memoriaRAM.jpg",
+                Nome = "Memória RAM Kingston 8GB DDR3 UTF 8 JAJA",
+                Descricao = "Memória RAM Kingston, excelente para jogos que exigem alto desempenho e performance",
+                Quantidade = 1,
+                ValorUnitario = 235.65,
+                ValorUnitarioTotal = "235,65"
+            });
+
+            lista.Add(new CarrinhoCompra()
+            {
+                Imagem = "/Content/img/microondas.jpg",
+                Nome = "Microondas Electrolux 1.8 TFF VGB COK",
+                Descricao = "Microondas da Electrolux de última geração, suporta as tecnologias BurnTheCock, que faz com que os alimentos esquentem mais rapidamente.",
+                Quantidade = 1,
+                ValorUnitario = 230.80,
+                ValorUnitarioTotal = "230,80"
+            });
+
+            lista.Add(new CarrinhoCompra()
+            {
+                Imagem = "/Content/img/relescopio.jpg",
+                Nome = "Telescópio Luna 580 PF66 55mm",
+                Descricao = "Telescópio com ótimo foco para enxergar a Lua, possui lentes que podem chegar até Marte!",
+                Quantidade = 1,
+                ValorUnitario = 640.90,
+                ValorUnitarioTotal = "640,90"
+            });
+
+            lista.Add(new CarrinhoCompra()
+            {
+                Imagem = "/Content/img/televisão.jpg",
+                Nome = "Televisão Samsung 32' Smart Screen, Smart Junk",
+                Descricao = "Televisão Samsung Smart Screen e Phone, com suporte a vídeos de 4K e muito mais!",
+                Quantidade = 1,
+                ValorUnitario = 1200.90,
+                ValorUnitarioTotal = "1200,90"
+            });
+
+            return View(lista);
         }
     }
 }
---
+++ Diamond/Models/CarrinhoCompra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diamond.Models
+{
+    public class CarrinhoCompra
+    {
+        public string Imagem { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public double ValorUnitario { get; set; }
+        public string ValorUnitarioTotal { get; set; }
+    }
+}
