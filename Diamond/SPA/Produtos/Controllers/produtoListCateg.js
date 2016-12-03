6d5373a97a2d0c931e1c44f0985adf14efb068f5
NickFelix <nicolas.felixa@gmail.com>
2016-12-03 16:23:49 -0200

    maisalteraçõse

--- Diamond/SPA/Produtos/Controllers/produtoListCateg.js
+++ Diamond/SPA/Produtos/Controllers/produtoListCateg.js
@@ -1,30 +1,36 @@
 ﻿angular.module('Diamond').controller('ProdutoCategoria', function ($scope, ProdutoCategoriaService, $routeParams, $location) {
     $scope.categoriaID = $routeParams.categoriaID;
     $scope.Produtos = [];
-    $scope.categoria = "";
+    $scope.categoria = "";   
 
     $scope.load = function () {
-        ProdutoCategoriaService.getProdutos($scope.categoriaID).then(function (retorno) {
-            $scope.Produtos = retorno.data
-        }).catch(function () { });
 
-        switch ($scope.categoriaID) {
-            case '1':
-                $scope.categoria = "DVDs e Blu-ray";
-                break;
-            case '2':
-                $scope.categoria = "Games";
-                break;
-            case '3':
-                $scope.categoria = "Celulares e Telefonia Fixa";
-                break;
-            case '4':
-                $scope.categoria = "Informática e Tablets";
-                break;
-            case '5':
-                $scope.categoria = "Tv, Aúdio e Home Theater";
-                break;
+        if ($scope.categoriaID) {
+            ProdutoCategoriaService.getProdutos($scope.categoriaID).then(function (retorno) {
+                $scope.Produtos = retorno.data
+            }).catch(function () { });
+
+            switch ($scope.categoriaID) {
+                case '1':
+                    $scope.categoria = "DVDs e Blu-ray";
+                    break;
+                case '2':
+                    $scope.categoria = "Games";
+                    break;
+                case '3':
+                    $scope.categoria = "Celulares e Telefonia Fixa";
+                    break;
+                case '4':
+                    $scope.categoria = "Informática e Tablets";
+                    break;
+                case '5':
+                    $scope.categoria = "Tv, Aúdio e Home Theater";
+                    break;
+            }
         }
+
+
+
     }
 
     $scope.exibirDetalhes = function (produto) {
