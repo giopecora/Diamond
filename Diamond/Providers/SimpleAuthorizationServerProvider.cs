40d3e4bb4f9b95bdf90a3b8a6b809ad968862595
Arthur <arthurluisrodrigues@hotmail.com>
2016-12-05 20:56:21 -0200

    sd

--- Diamond/Providers/SimpleAuthorizationServerProvider.cs
+++ Diamond/Providers/SimpleAuthorizationServerProvider.cs
@@ -1,7 +1,9 @@
 ﻿using Diamond.Business.Business;
 using Diamond.Domain.DTO;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -11,16 +13,28 @@ namespace Diamond.Providers
     {
         public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
+            string clientId = string.Empty;
+            string clientSecret = string.Empty;
+
+            if(!context.TryGetBasicCredentials(out clientId, out clientSecret))
+            {
+                context.TryGetFormCredentials(out clientId, out clientSecret);
+            }
+
+            //context.OwinContext.Set<string>("as:clientAllowedOrigin", client.alo)
+
             context.Validated();
         }
 
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
+            var allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin");
+
+            if (allowedOrigin == null) allowedOrigin = "*";
+
             context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
 
-            AuthenticationBusiness auth = new AuthenticationBusiness();
-
-            UsuarioDTO user = auth.FindUser(context.UserName, context.Password);
+            UsuarioDTO user = new AuthenticationBusiness().FindUser(context.UserName, context.Password);
 
             if (user == null)
             {
@@ -30,10 +44,28 @@ namespace Diamond.Providers
 
             var identity = new ClaimsIdentity(context.Options.AuthenticationType);
 
+            identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
             identity.AddClaim(new Claim("sub", context.UserName));
             identity.AddClaim(new Claim("perfis", user.PerfisToJson()));
 
-            context.Validated(identity);
+            var props = new AuthenticationProperties(new Dictionary<string, string>
+            {
+                { "perfis", user.PerfisToJson() }
+            });
+
+            var ticket = new AuthenticationTicket(identity, props);
+
+            context.Validated(ticket);
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach(KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return base.TokenEndpoint(context);
         }
     }
 }
